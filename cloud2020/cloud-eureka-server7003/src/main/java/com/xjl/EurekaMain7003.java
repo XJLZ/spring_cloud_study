53255ffebafc4444c196ba17b30ab3ad00930b67
XJL <xxxx@xxxx.com>
2020-09-03 19:20:39 +0800

    init

---
+++ cloud2020/cloud-eureka-server7003/src/main/java/com/xjl/EurekaMain7003.java
@@ -0,0 +1,19 @@
+package com.xjl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: 玲
+ * @Description:
+ * @create 2020-09-02 17:13
+ * @Modified By:
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7003 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7003.class,args);
+    }
+}
--- cloud2020/cloud-provider-payment8001/src/main/java/com/xjl/service/PaymentService.java
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/xjl/service/PaymentService.java
@@ -2,7 +2,6 @@ package com.xjl.service;
 
 
 import com.xjl.domain.Payment;
-import org.apache.ibatis.annotations.Param;
 
 /**
  * @Author: 玲
@@ -10,8 +9,9 @@ import org.apache.ibatis.annotations.Param;
  * @create 2020-08-10 17:36
  * @Modified By:
  */
-public interface PaymentService {
+public interface PaymentService{
+
     int create(Payment payment);
 
-    Payment getPaymentById(@Param("id") Long id);
+    Payment getPaymentById(Long id);
 }
